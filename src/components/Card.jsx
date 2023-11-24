dd110fe860e0a1f5ec9e7dbed3aaddd4c019a0d4
Maxime <maxdevvynter@gmail.com>
2023-11-24 12:36:45 +0100

    Create Card

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+import "../main.scss"
+
+const Card= () => {
+    return (
+        <article className="Card">
+            <h2 className="Card_Title">Titre de la location</h2>
+        </article>        
+    )
+}
+export default Card
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,23 @@
 import Header from  "../layout/Header"
 import Banner from  "../components/Banner"
+import Card from  "../components/Card"
 
 const Home = () => {
     return (
         <div className="Home">
             <Header />
-            <Banner />
+            <main>
+                <Banner /> 
+                <div className="Card-Container">
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
+            </main>
+            
         </div>
     )
 }
