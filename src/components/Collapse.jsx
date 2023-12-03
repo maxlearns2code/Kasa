83f4accabfa8ab56b088f7b26c10062fe9353917
Maxime <maxdevvynter@gmail.com>
2023-12-03 15:47:32 +0100

    add useState + animations on collapse

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,15 +1,28 @@
 import "../main.scss"
 import ArrowCollapse from "../assets/Arrow-Collapse.svg"
+import { useState } from 'react'
 
 const Collapse = ({data}) => {
-    return (        
+    const [collapse, showText] = useState(false)
+    return collapse ? (        
         <article className="collapse">
             <div className="collapse_header">
-                <h2 className="collapse_header_title">{data.title}</h2>
-                <img src={ArrowCollapse} alt="ouverture du menu"/>
-            </div>            
+                <h2 className="collapse_header--title">{data.title}</h2>
+                <button className="collapse_header--downbtn"onClick={() => showText(false)}>
+                    <img src={ArrowCollapse} alt="fermeture du menu"/>
+                </button>
+            </div>
             <p className="collapse_text">{data.text}</p>
         </article>        
+    ) : (
+        <article className="collapse">
+            <div className="collapse_header">
+                <h2 className="collapse_header--title">{data.title}</h2>
+                <button className="collapse_header--upbtn"onClick={() => showText(true)}>
+                    <img src={ArrowCollapse} alt="ouverture du menu"/>
+                </button>
+            </div>
+        </article> 
     )
 }
 export default Collapse
